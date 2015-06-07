89b002c4b5ef386fc3d9b0ad9dd7924518a8593e
Sam Meyers <sameyers@yahoo.com>
2015-06-07 13:17:34 -0500

    Add image upload with Paperclip.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,20 @@
 class Pin < ActiveRecord::Base
-	belongs_to :user
+belongs_to :user
+
+has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+end
+
+require 'paperclip/media_type_spoof_detector'
+
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
+# follow this with the class
+class Pin < ActiveRecord::Base
+#contents of your current model.
 end
