249894ed52a1fe4f49104577902c9860d07e1389
Sam Meyers <sameyers@yahoo.com>
2015-06-10 22:14:07 -0500

    Disply name instead of email.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,6 +3,8 @@ belongs_to :user
 
 has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+validates :image, presence: true
+validates :description, presence: true
 end
 
 require 'paperclip/media_type_spoof_detector'
